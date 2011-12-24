abbb62223dbaac2637a41b020489dc3e6096e1d7
Guy Halford-Thompson <guy@cach.me>
2011-12-23 16:11:56 -0800

    Initial working commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var glog = require('./lib/glog'),
+	connect = require('connect'),
+	path = require('path');
+
+
+glog.rebuild(function(options) {
+
+	console.log('Starting server on port ' + options.port);
+
+	var server = connect.createServer(
+		connect.static(path.join(options.repository, '/public')),
+		connect.staticCache(),
+		connect.router(function(app) {
+			app.get('/', glog.req_home);
+			app.get('/__render', function(req, res, next) {
+				glog.rebuild(function(options) {
+					res.end();
+				});
+			});
+			app.get('/:article', glog.req_article);
+		})
+	).listen(options.port);
+});
